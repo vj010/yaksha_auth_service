6e24d0082c5b27a5d3d544772880685828c0da42
vijay <vjblacklight@gmail.com>
2022-07-21 09:16:53 +0530

    MODIFY - refactored index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,19 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express, { urlencoded } from 'express';
 import { AppContext } from './utils/app';
-import dotenv from 'dotenv';
+
 import { AuthenticationController } from './authentication/authentication.controller';
 import { AuthenticationService } from './authentication/authentication.service';
+import { googleOAuthConfig } from './config/google-oauth-config';
 
 const appContext: AppContext = new AppContext();
-dotenv.config();
 
 appContext.addMiddleWare(express.json(), urlencoded({ extended: true }));
 
 appContext.setControllerRoutes(
-  new AuthenticationController(
-    new AuthenticationService(
-      process.env.GOOGLE_CLIENT_ID,
-      process.env.GOOGLE_CLIENT_SECRET,
-    ),
-  ),
+  new AuthenticationController(new AuthenticationService(googleOAuthConfig)),
 );
 
 appContext.startServer(parseInt(process.env.PORT));
